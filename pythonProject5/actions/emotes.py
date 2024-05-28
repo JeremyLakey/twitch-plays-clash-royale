f10210e0f8f284116e29c94fb56f815f48466cab
personman61 <jeremysl123@gmail.com>
2024-05-27 22:22:46 -0600

    Set up project

---
+++ pythonProject5/actions/emotes.py
@@ -0,0 +1,62 @@
+import pyautogui
+import time
+
+# emote button
+# Point(x=706, y=895)
+
+# emote 1
+# Point(x=801, y=717)
+def emote_1():
+    pyautogui.click(706,895)
+    pyautogui.click(801,717)
+    pyautogui.moveTo(100,100)
+
+
+# emote 2
+# Point(x=906, y=720)
+def emote_2():
+    pyautogui.click(706,895)
+    pyautogui.click(906,717)
+    pyautogui.moveTo(100,100)
+
+# emote 3
+# Point(x=1006, y=723)
+def emote_3():
+    pyautogui.click(706,895)
+    pyautogui.click(1006,717)
+    pyautogui.moveTo(100,100)
+
+# emote 4
+# Point(x=1112, y=722)
+def emote_4():
+    pyautogui.click(706,895)
+    pyautogui.click(1112,717)
+    pyautogui.moveTo(100,100)
+
+# emote 5
+# Point(x=799, y=805)
+def emote_5():
+    pyautogui.click(706,895)
+    pyautogui.click(799,805)
+    pyautogui.moveTo(100,100)
+
+# emote 6
+# Point(x=901, y=809)
+def emote_6():
+    pyautogui.click(706,895)
+    pyautogui.click(901,809)
+    pyautogui.moveTo(100,100)
+
+# emote 7
+# Point(x=1005, y=810)
+def emote_7():
+    pyautogui.click(706,895)
+    pyautogui.click(1005,810)
+    pyautogui.moveTo(100,100)
+
+# emote 8
+# Point(x=1111, y=805)
+def emote_8():
+    pyautogui.click(706,895)
+    pyautogui.click(1111,805)
+    pyautogui.moveTo(100,100)
---
+++ pythonProject5/main.py
@@ -0,0 +1,31 @@
+import pyautogui
+import time
+
+from actions.emotes import *
+
+def main():
+    while True:
+        time.sleep(2)
+        print(pyautogui.position())
+
+def main2():
+    while True:
+        time.sleep(2)
+        emote_1()
+        time.sleep(2)
+        emote_2()
+        time.sleep(2)
+        emote_3()
+        time.sleep(2)
+        emote_4()
+        time.sleep(2)
+        emote_5()
+        time.sleep(2)
+        emote_6()
+        time.sleep(2)
+        emote_7()
+        time.sleep(2)
+        emote_8()
+
+
+main2()
