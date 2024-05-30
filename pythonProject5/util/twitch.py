ee3327589667b5ba1bfeb5b8adf6048e351ddf2d
personman61 <jeremysl123@gmail.com>
2024-05-29 23:47:30 -0600

    basic .5 event loop for making actions

--- pythonProject5/util/twitch.py
+++ pythonProject5/util/twitch.py
@@ -1,15 +1,23 @@
 from twitchio import Message
 from twitchio.client import Client
+import asyncio
 from twitchToken import setup_token, getConfig
 
+def eventLoop():
+    print("Helloooo")
+    loop.call_later(.5, eventLoop)
 
+loop = asyncio.new_event_loop()
+loop.call_later(.5, eventLoop)
+
+# https://twitchio.dev/en/stable/twitchio.html#twitchio.Client.event_message
 class Bot(Client):
 
     def __init__(self):
         # To get more tokens
         # https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=<your client id>&redirect_uri=https://localhost:3000&scope=chat%3Aread+chat%3Aedit
         #
-        super().__init__(token=getConfig("token"), initial_channels=['personman61'])
+        super().__init__(token=getConfig("token"), initial_channels=['personman61'], loop=loop)
 
     async def event_ready(self):
         # Notify us when everything is ready!
